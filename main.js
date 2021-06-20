03f996cfb4c16f0c2d97e04a878ca8df81ffb960
Aaradhana Bharathwaj <73657462+aaradhanabharathwaj@users.noreply.github.com>
2021-06-20 11:50:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+left_wrist=0;
+right_wrist=0;
+difference=0;
 function setup(){
     video=createCapture(VIDEO);
     video.size(450,400);
@@ -8,6 +11,10 @@ function setup(){
 }
 function draw(){
     background("#667e9b");
+    fill("#daf3f7");    
+    fill("#ebf2f2");
+    text("Aaradhana Bharathwaj", 50,50);
+    textSize(difference);
 }
 function modelLoaded(){
     console.log("PoseNet is inisialized !!")
@@ -15,5 +22,10 @@ function modelLoaded(){
 function gotPoses(results){
     if (results.length>0){
         console.log(results);
+        left_wrist=results[0].pose.leftWrist.x;
+        right_wrist=results[0].pose.rightWrist.x;
+        console.log("Left Wrist = "+left_wrist+"Right Wrist = "+right_wrist);
+        difference=floor(left_wrist-right_wrist);
+        console.log(left_wrist,right_wrist,difference)
     }
 }
